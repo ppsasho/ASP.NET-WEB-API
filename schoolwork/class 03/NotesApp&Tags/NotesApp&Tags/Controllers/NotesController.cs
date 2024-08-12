3b85bf50d5016c059e9a58ae37fbe2f3bc658e1b
ppsasho <businessmail5545@gmail.com>
2024-08-12 15:12:36 +0200

    - schoolwork stuff

--- schoolwork/class 03/NotesApp&Tags/NotesApp&Tags/Controllers/NotesController.cs
+++ schoolwork/class 03/NotesApp&Tags/NotesApp&Tags/Controllers/NotesController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using NotesApp_Tags.Db;
 using NotesApp_Tags.Models;
+using NotesApp_Tags.Models.Enums;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
 
 namespace NotesApp_Tags.Controllers
 {
@@ -23,12 +27,37 @@ namespace NotesApp_Tags.Controllers
             return Ok(StaticDb.Notes[index-1]);
         }
 
-        [HttpGet]
-        public ActionResult<Note> GetFromQueryString(string index) 
+        [HttpGet("getFromQueryString")]
+        public ActionResult<Note> GetFromQueryString([FromQuery] int index)
         {
-            return Ok();
-        }
+            try
+            {
+                if (index <= 0) return BadRequest("Please don't request using a negative or zero value index!");
+                if (index > StaticDb.Notes.Count) return StatusCode(StatusCodes.Status404NotFound, "Nonexistent index!");
 
+                return Ok(StaticDb.Notes[index - 1]);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Please notify us about this error through our support page");
+            }
+        }
+        [HttpGet("({text}/priority/{priority}")]
+        public ActionResult<List<Note>> FilterNotes(string? text, int? priority)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(text) || (priority == null || priority < 0 || priority > 3)) 
+                {
+                    return Ok(StaticDb.Notes);
+                }
+                if (string.IsNullOrEmpty(text)) return Ok(StaticDb.Notes.Where(x => priority == (int)x.Priority));
+                return Ok(StaticDb.Notes.Where(x => x.Text.ToLower().Contains(text.ToLower()) && priority == (int)x.Priority));
+            } catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Please notify us about this error through our support page");
+            }
+        }
 
     }
 }
